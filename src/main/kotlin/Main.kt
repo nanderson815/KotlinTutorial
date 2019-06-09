57e2a7ea67b4e1ae13962e01b9358055b3d19fe8
Noah A <nanderson815@gmail.com>
2019-06-09 11:28:39 -0400

    dat 2 kotlin warmup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,18 +2,18 @@ fun main(args: Array<String>){
     println("Hello World!")
     println("My first Kotlin Program!")
 
-    var noah = "Noah Anderson"
+    val noah = "Noah Anderson"
     println("The name of our employee is $noah")
 
-    var noahWeeklySalary: Int = 200
-    var noahMonthlySalary: Int = noahWeeklySalary * 4
+    val noahWeeklySalary: Int = 200
+    val noahMonthlySalary: Int = noahWeeklySalary * 4
     println("$noah's monthly salary is $$noahMonthlySalary")
 
     println()
 
     val apples: Int = 6
     val oranges: Double = 5.0
-    var fruit: Double = (apples + oranges)
+    val fruit: Double = (apples + oranges)
     println("The total amount of apples and oranges is $fruit")
 
     val weeks = 234

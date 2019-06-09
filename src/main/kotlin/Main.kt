9075faafd26408ecfb2273eaefbe1a06c255f6f8
Noah A <nanderson815@gmail.com>
2019-06-09 11:43:49 -0400

    kotlin training day 2: conditionals (yawn)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,12 @@
-fun main(args: Array<String>){
-    println("Hello World!")
-    println("My first Kotlin Program!")
+fun main(Args: Array<String>) {
+    val lives = 3
 
-    val noah = "Noah Anderson"
-    println("The name of our employee is $noah")
+    var isGameOver = (lives < 1)
+    println(isGameOver)
 
-    val noahWeeklySalary: Int = 200
-    val noahMonthlySalary: Int = noahWeeklySalary * 4
-    println("$noah's monthly salary is $$noahMonthlySalary")
-
-    println()
-
-    val apples: Int = 6
-    val oranges: Double = 5.0
-    val fruit: Double = (apples + oranges)
-    println("The total amount of apples and oranges is $fruit")
-
-    val weeks = 234
-    val years: Double = weeks / 52.0
-
-    println("$weeks weeks is $years years. ")
-
+    if (isGameOver) {
+        println("Game over!")
+    } else {
+        println("You're still alive!")
+    }
 }

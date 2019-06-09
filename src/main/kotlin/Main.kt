148782a4b8a920020298e0f5ad0458d7bbbe5d9c
Noah A <nanderson815@gmail.com>
2019-06-09 11:53:28 -0400

    added new git ignore

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,26 @@
 fun main(Args: Array<String>) {
-    val lives = 3
+//    val lives = 3
+//
+//    var isGameOver = (lives < 1)
+//    println(isGameOver)
+//
+//    if (isGameOver) {
+//        println("Game over!")
+//    } else {
+//        println("You're still alive!")
+//    }
 
-    var isGameOver = (lives < 1)
-    println(isGameOver)
+    println("How old are you?: ")
+    val age = readLine()!!.toInt()
+    println("Age is: $age")
 
-    if (isGameOver) {
-        println("Game over!")
-    } else {
-        println("You're still alive!")
+    val message: String
+    message = when {
+        age < 18 -> "You're too young to vote."
+        age == 100 -> "Dang you are old."
+        else -> "You are old enough to vote."
     }
+
+    println(message)
+
 }

a14dc188af82c0819e7de497fc2b2dd6a4428443
Noah A <nanderson815@gmail.com>
2019-06-10 20:14:08 -0400

    completed class challenge for day 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,4 +10,16 @@ fun main(args: Array<String>) {
 
     val ryan = Player("Ryan", 2,5,1000)
     ryan.show()
-} 
+
+    val axe = Weapon("Axe", 12)
+    ryan.weapon = axe
+    println(ryan.weapon.name)
+
+    axe.damageInflicted = 24
+    println(axe.damageInflicted)
+
+    println(ryan.weapon.damageInflicted)
+
+    tim.weapon = Weapon("Sword", 10)
+    println(tim.weapon.name)
+}
